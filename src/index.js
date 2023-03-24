7988053287aabd02a6aa4d237591fd7c2a6e59ca
juancub <juanuribe.ber@gmail.com>
2023-03-23 20:48:20 -0500

    agregando imagenes a el store estatico

--- src/index.js
+++ src/index.js
@@ -5,4 +5,8 @@ import { createRoot } from 'react-dom/client';
 
 const container = document.getElementById('root');
 const root = createRoot(container); // createRoot(container!) if you use TypeScript
-root.render(<App tab="home" />);
+root.render(
+<App 
+tab="home" 
+/>
+);
